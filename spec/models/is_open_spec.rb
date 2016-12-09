fdadee4b4ed9ef13df83db65839ea02c7e2674b6
Conor Landry <clandry94@ufl.edu>
2016-12-09 15:19:09 -0500

    Added tests for associations

--- spec/models/is_open_spec.rb
+++ spec/models/is_open_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe IsOpen, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:map_object) }
+  it { should belong_to(:restaurant) }
 end
